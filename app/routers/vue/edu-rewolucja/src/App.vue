73e3ed2e897a9dc9d13e4f494be22aa19a152d50
Waver <kacpermachay@gmail.com>
2022-08-02 13:02:31 +0200

    big update - added vue with node

---
+++ app/routers/vue/edu-rewolucja/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  {{ title }}
+  <TestItem />
+</template>
+
+<script>
+import TestItem from './components/test-item.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestItem
+},
+  data() {
+    return {
+      title: 'My first title'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/routers/vue/edu-rewolucja/src/components/test-item.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Test czy działa</h1>
+</template>
