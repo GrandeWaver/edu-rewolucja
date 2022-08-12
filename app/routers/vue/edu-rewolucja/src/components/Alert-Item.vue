04830824567cb57cb9e5a100ae831d99240cfa27
Waver <kacpermachay@gmail.com>
2022-08-12 17:19:18 +0200

    add alert

---
+++ app/routers/vue/edu-rewolucja/src/components/Alert-Item.vue
@@ -0,0 +1,49 @@
+<template>
+    <div v-if="show" class="alert">
+        <div class="textdetails">
+            Strona w trakcie budowy. Nie wszystko może działać poprawnie :(
+        </div>
+        <div @click="closeAlert" class="alertcancel">X</div>
+    </div>
+    <br v-if="show">
+</template>
+
+<script>
+export default {
+    // props: ['isAuthenticated', 'userData'],
+    data() {
+        return {
+            show: true
+        }
+    },
+    methods: {
+        closeAlert() {
+            this.show = false
+        },
+}
+}
+</script>
+
+<style>
+.alert{
+    position:absolute;
+    top:35px;
+    text-align: center;
+    color: white;
+    width: 100%;
+    margin: 0;
+}
+.textdetails{
+    background-color: #FF6E6E;
+    float: left;
+    padding: 4px;
+    text-align: center;
+    width: 100%;
+}
+.alertcancel{
+    text-align: right;
+    padding: 6px;
+    transform: translateY(-22px);
+    cursor: pointer;
+}
+</style>
