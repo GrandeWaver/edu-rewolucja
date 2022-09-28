d8937669457aa3e73c53bc285488c8f4876a198b
Waver <kacpermachay@gmail.com>
2022-09-28 13:34:55 +0200

    videocall update

--- app/routers/vue/edu-rewolucja/src/components/VideoCall-Item.vue
+++ app/routers/vue/edu-rewolucja/src/components/VideoCall-Item.vue
@@ -16,7 +16,7 @@
           <img src="@/assets/add_to_zoom-unavailable.png" height="32" alt="Dołącz"/>
         </span>
         <span v-if="$root.userData.account_type == 'tutor'" class="pointer">
-          <a href="https://zoom.us/oauth/authorize?response_type=code&client_id=YsmfYSibRAOiduiok13lPg&redirect_uri=https://app.edu-rewolucja.pl/auth/zoomuser/{{ data.lesson_id }}" target="_blank" rel="noopener noreferrer"><img src="@/assets/add_to_zoom.png" height="32" alt="Dołącz" /></a>
+          <a :href="'https://zoom.us/oauth/authorize?response_type=code&client_id=YsmfYSibRAOiduiok13lPg&redirect_uri=https://app.edu-rewolucja.pl/auth/zoomuser/'+data.lesson_id" target="_blank" rel="noopener noreferrer"><img src="@/assets/add_to_zoom.png" height="32" alt="Dołącz" /></a>
         </span>
     </div>
 </div>
