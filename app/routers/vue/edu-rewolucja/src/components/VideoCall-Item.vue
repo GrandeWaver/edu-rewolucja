168a31ae3b898b5de2a670cdbf8c8f5613beeaa3
Waver <kacpermachay@gmail.com>
2022-09-28 16:28:07 +0200

    videocall update

--- app/routers/vue/edu-rewolucja/src/components/VideoCall-Item.vue
+++ app/routers/vue/edu-rewolucja/src/components/VideoCall-Item.vue
@@ -12,12 +12,14 @@
               Kliknij, aby utworzyć spotkanie z {{ data.notification.student_firstname }} {{ data.notification.student_lastname }}.
             </span>
         </div>
-        <span v-if="$root.userData.account_type == 'student' && data.notification.status == 'not ready' " class="notAllowed">
-          <img src="@/assets/add_to_zoom-unavailable.png" height="32" alt="Dołącz"/>
-        </span>
-        <span v-if="$root.userData.account_type == 'student' && data.notification.status == 'ready' " class="pointer">
-          {{ data.join_url }}
-          <a :href="data.join_url">Dołącz</a>
+        <span v-if="$root.userData.account_type == 'student'">
+            <span v-if="data.notification.status == 'ready' " class="pointer">
+              {{ data.join_url }}
+              <a :href="data.join_url">Dołącz</a>
+            </span>
+            <span v-else class="notAllowed">
+              <img src="@/assets/add_to_zoom-unavailable.png" height="32" alt="Dołącz"/>
+            </span>
         </span>
         <span v-if="$root.userData.account_type == 'tutor'" class="pointer">
           <a :href="'https://zoom.us/oauth/authorize?response_type=code&client_id=YsmfYSibRAOiduiok13lPg&redirect_uri=https://app.edu-rewolucja.pl/auth/zoomuser/'+data.lesson_id" target="_blank" rel="noopener noreferrer"><img src="@/assets/add_to_zoom.png" height="32" alt="Dołącz" /></a>
